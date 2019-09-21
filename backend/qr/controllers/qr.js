75117d3285963fa1bc19d7a59ca4d79a61eeb370
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-09-21 14:44:39 -0500

    :sparkles: POST api/qr returns generated QR code to client

--- backend/qr/controllers/qr.js
+++ backend/qr/controllers/qr.js
@@ -1,9 +1,29 @@
 const express = require("express");
-
 const router = express.Router();
 
-router.route("/:venue_id/item/:item_id").post(async (req, res) => {
-  const { venue_id, item_id } = req.params;
+const QRCode = require("qrcode");
+
+router.route("/").post(async (req, res) => {
+  const { venue_id, item_id } = req.body;
+  if (!venue_id || !item_id) {
+    return res
+      .status(400)
+      .json({ message: "Request must include venue_id and item_id." });
+  }
+
+  try {
+    const qr_code = await QRCode.toDataURL(`${venue_id},${item_id}`);
+
+    // // upload image to s3 and store response into const
+    // const location = await uploadQR()
+    // if (location) {
+    //     // post to database
+    // }
+    res.status(200).json({ qr_code });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
 });
 
 module.exports = router;
