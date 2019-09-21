35da0ff36245e157caa40fc110e1b011cd69f48a
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-09-21 14:21:53 -0500

    :construction: Connecting qr router to server
    Init qr controller to handle post request with venue and item id

--- backend/qr/index.js
+++ backend/qr/index.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const server = require("./server");
 
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
-  console.log(`\n=== Server running on port ${PORT} ===\n`);
+server.listen(port, () => {
+  console.log(`\n=== Server running on port ${port} ===\n`);
 });
